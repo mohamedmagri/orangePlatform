9444ab94fb08b814721ef38e8d84f24baa11a4c5
mohamedmagri <mohamed.magri@esprit.tn>
2024-05-10 19:10:41 +0200

    data validation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from orangePlatform import logger
 from orangePlatform.pipeline.stage01dataIngestion import DataIngestionTrainingPipeline
+from orangePlatform.pipeline.stage02dataValidation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -11,3 +12,13 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+STAGE_NAME = "Data validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
